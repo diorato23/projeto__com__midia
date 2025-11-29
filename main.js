d5d39023d27a82136bcc2ccb12d96f64d4760441
Rafael Diorato <diorato@live.com>
2025-11-29 12:24:47 -0500

    Atualizando projeto

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-document.querySelector('.tecla_pom')
+function sonidoPlayPom() {
+    document.querySelector('#sonido_tecla_pom').play();
+    
+}
+
+function playSonidoClap() {
+    document.querySelector('#sonido_tecla_clap').play();
+}
+
+
+document.querySelector('.tecla_pom').onclick = sonidoPlayPom;
+document.querySelector('.tecla_clap').onclick = playSonidoClap;
