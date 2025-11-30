98e03a5e4c149cc956f641d93f1599235b5eef19
Rafael Diorato <diorato@live.com>
2025-11-30 13:24:35 -0500

    Finalizando o projeto

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-function sonidoPlayPom() {
-    document.querySelector('#sonido_tecla_pom').play();
+function playSonido (idElemento){
+    document.querySelector(idElemento).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < 9) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento =  tecla.classList[1];
+    
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+
+    tecla.onclick = function (){
+        playSonido(idAudio);
+    };
+    contador += 1;
     
 }
-
-function playSonidoClap() {
-    document.querySelector('#sonido_tecla_clap').play();
-}
-
-
-document.querySelector('.tecla_pom').onclick = sonidoPlayPom;
-document.querySelector('.tecla_clap').onclick = playSonidoClap;
