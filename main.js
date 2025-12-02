9de731c083bfbee2657a544e645f9c855c4ce62a
Rafael Diorato <diorato@live.com>
2025-12-01 23:25:00 -0500

    Projeto Finalizado

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-function playSonido (idElemento){
-    document.querySelector(idElemento).play();
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < 9) {
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
-    const instrumento =  tecla.classList[1];
-    
+    const instrumento = tecla.classList[1];
+    console.log(contador)
 
     const idAudio = `#sonido_${instrumento}`;
     console.log(idAudio)
@@ -17,6 +16,18 @@ while (contador < 9) {
     tecla.onclick = function (){
         playSonido(idAudio);
     };
-    contador += 1;
-    
+    tecla.onkeydown = function(evento){
+        
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');
+        }else {
+
+            tecla.onkeyup = function(){
+                tecla.classList.remove('activa');
+        }
+        
+    }
+}
+   
+
 }
